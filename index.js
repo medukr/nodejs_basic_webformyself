2a0da758b19149dee19b2f8ef75e5c8c18551c4c
Andrii Demydiuk <andrii.demydyuk@gmail.com>
2021-07-11 12:38:50 +0300

    http.server

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-console.log('Hello', __filename)
+const http = require('http')
 
-// const userObj = require('./user')
-// console.log(userObj);
+const server = http.createServer((request, responce) => {
+    console.log(request.url);
 
-const obj = require('./user')
+    responce.write('<h1>Hello from HodeJS</h1>')
+    responce.write('<h2>Hello from HodeJS</h2>')
+    responce.write('<h3>Hello from HodeJS</h3>')
+    responce.end(`
+        <div style="background:red; width:200px; height:200px;">
+            <h1>Test</h1>
+        </div>
+    `)
+})
 
-
-console.log(obj.user)
-
-obj.sayHello()
+server.listen(3000, () => {
+    console.log('Server is running...');
+})
